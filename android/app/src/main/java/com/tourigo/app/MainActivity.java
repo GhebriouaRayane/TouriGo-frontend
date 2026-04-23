b5ba49c7469cd5ceddff1f5cfa1b4b7de57845fc
GhebriouaRayane <rayaneghebrioua10@gmail.com>
2026-04-23 13:14:41 +0200

    Ajout des fichiers Android et Capacitor

---
+++ android/app/src/main/java/com/tourigo/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tourigo.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tourigo.app',
+  appName: 'tourigo-apk',
+  webDir: 'dist'
+};
+
+export default config;
